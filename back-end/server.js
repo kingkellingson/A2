1c1c56eb1d4c9e69353213a67db42be132bae6ac
kingkellingson@gmail.com <kingkellingson@gmail.com>
2021-08-28 12:47:39 -0600

    lesson3

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+// setup express
+const app = express();
+
+// setup body parser middleware to conver to JSON and handle URL encoded forms
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// connect to the mongodb database
+mongoose.connect('mongodb://localhost:27017/pagliaccio', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+
+// import the users module and setup its API path
+const users = require("./users.js");
+app.use("/api/users", users.routes);
+
+
+// listen on port 3000
+app.listen(3000, () => console.log('Server listening on port 3000!'));
 
