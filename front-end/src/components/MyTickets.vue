37e06bf8991ee53148bff11470d45083c82c6a09
kingkellingson@gmail.com <kingkellingson@gmail.com>
2021-08-28 13:37:12 -0600

    finsih lesson 3?

---
+++ front-end/src/components/MyTickets.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="main">
+  <h2>Logged in as: {{user.firstName}} {{user.lastName}}</h2>
+  <h1>My Tickets</h1>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'MyTickets',
+  computed: {
+    user() {
+      return this.$root.$data.user;
+    }
+  }
+}
+</script>
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
 <div>
-  <HomePage />
+  <MyTickets v-if="user" />
+  <HomePage v-else />
 </div>
 </template>
 
+
+
 <script>
 import HomePage from '@/components/HomePage.vue'
+import MyTickets from '@/components/MyTickets.vue'
 export default {
   name: 'home',
   components: {
-    HomePage
+    HomePage,
+    MyTickets
+  },
+  computed: {
+    user() {
+      return this.$root.$data.user;
+    }
   }
 }
 </script>
+
+
