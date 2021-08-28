4ef27a16205facc79126662c453b0d7b9576a70b
kingkellingson@gmail.com <kingkellingson@gmail.com>
2021-08-28 14:29:28 -0600

    logout

--- front-end/src/components/MyTickets.vue
+++ front-end/src/components/MyTickets.vue
@@ -1,18 +1,29 @@
 <template>
 <div class="main">
-  <h2>Logged in as: {{user.firstName}} {{user.lastName}}</h2>
+  <h2>Logged in as: {{user.firstName}} {{user.lastName}} <button @click="logout" class="pure-button pure-button-primary">Logout</button></h2>
   <h1>My Tickets</h1>
 </div>
 </template>
 
 
 <script>
+import axios from 'axios';
 export default {
   name: 'MyTickets',
   computed: {
     user() {
       return this.$root.$data.user;
     }
+  },
+    methods: {
+    async logout() {
+      try {
+        await axios.delete("/api/users");
+        this.$root.$data.user = null;
+      } catch (error) {
+        this.$root.$data.user = null;
+      }
+    },
   }
 }
 </script>
